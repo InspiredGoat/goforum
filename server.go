55c4aced78a7a1241ecc0ddc9e8db93927b7ee36
inspiredgoat <tomd@airmail.cc>
2023-05-21 21:29:11 +1000

    many more changes, user auth works now

--- server.go
+++ server.go
@@ -1,23 +1,26 @@
 package main
 
 import (
-	// "crypto/sha256"
+	"crypto/sha256"
+	"encoding/base64"
 	"encoding/json"
 	"errors"
 	"fmt"
+	"html/template"
 	"io/ioutil"
 	"math/rand"
 	"net/http"
+	"os"
 	"strings"
 	"time"
 )
 
-func get_authcode(r *http.Request) string {
+func authcode_from_request(r *http.Request) string {
 	cookies := r.Cookies()
 	var authcode string
 	for i := 0; i < len(cookies); i++ {
 		if cookies[i].Name == "authcode" {
-			fmt.Println(cookies[i].Value)
+			// fmt.Println(cookies[i].Value)
 			authcode = cookies[i].Value
 		}
 	}
@@ -51,26 +54,26 @@ func requestPathToStrings(r *http.Request) []string {
 // file with used codes
 // file with available codes
 
-type ActiveUser struct {
+type User struct {
 	Name     string
 	IsAdmin  bool
 	Authcode string
 }
 
-var active_users []ActiveUser
+var active_users []User
 
 func salt_and_hash(salt string, password string, username string) string {
-	// h := sha512.New()
-	// h.Write([]byte(password))
-	// h.Write([]byte(username))
-	// s := h.Sum([]byte(salt))
+	h := sha256.New()
+	h.Write([]byte(password))
+	h.Write([]byte(username))
+	s := h.Sum([]byte(salt))
 
-	return password
+	return base64.StdEncoding.EncodeToString(s)
 }
 
 var POSSIBLE_ID_CHARACTERS = []byte("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789+/")
 
-func gen_rand_id(length int) string {
+func base64_random(length int) string {
 	bytes := make([]byte, length)
 
 	for i := 0; i < length; i++ {
@@ -80,9 +83,9 @@ func gen_rand_id(length int) string {
 	return string(bytes[:length])
 }
 
-func get_user_from_authcode(authcode string) (ActiveUser, error) {
+func user_from_authcode(authcode string) (User, error) {
 	if len(authcode) < 128 {
-		return ActiveUser{}, errors.New("Invalid authcode")
+		return User{}, errors.New("Invalid authcode")
 	}
 
 	for i := 0; i < len(active_users); i++ {
@@ -90,15 +93,67 @@ func get_user_from_authcode(authcode string) (ActiveUser, error) {
 			return active_users[i], nil
 		}
 	}
-	return ActiveUser{}, errors.New("Invalid authcode")
+	return User{}, errors.New("Invalid authcode")
 }
 
-func user_login(in_username string, in_password string) (user ActiveUser, error error) {
-	// open accounts  file
+func file_lines(filename string) []string {
 	a_bytes, _ := ioutil.ReadFile("accounts.csv")
 	a_string := string(a_bytes)
 	rows := strings.Split(a_string, "\n") // ignore header bit of the csv
 	rows = rows[1 : len(rows)-1]
+
+	return rows
+}
+
+// func users_list() []User {
+
+// }
+
+func user_is_name_unique(new_name string) bool {
+	rows := file_lines("accounts.csv")
+
+	for i := 0; i < len(rows); i++ {
+		row := rows[i]
+		cols := strings.Split(row, ",")
+		if cols[0] == new_name {
+			return false
+		}
+	}
+
+	return true
+}
+
+func user_add(username string, password string, is_admin bool, code string) {
+	salt := base64_random(16)
+	hashed_password := salt_and_hash(salt, password, username)
+
+	f, _ := os.OpenFile("accounts.csv", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+
+	admin_str := "no"
+	if is_admin {
+		admin_str = "yes"
+	}
+
+	f.Write([]byte(username + "," + hashed_password + "," + salt + "," + admin_str + "," + code + "\n"))
+
+	f.Close()
+}
+
+func user_remove(username string) {
+	lines := file_lines("accounts.csv")
+
+	newdata := ""
+	for i := 0; i < len(lines); i++ {
+		if strings.SplitN(lines[i], ",", 1)[0] != username {
+			newdata += lines[i]
+		}
+	}
+
+	ioutil.WriteFile("accounts.csv", []byte(newdata), 0644)
+}
+
+func user_login(in_username string, in_password string) (user User, error error) {
+	rows := file_lines("accounts.csv")
 
 	for i := 0; i < len(rows); i++ {
 		row := rows[i]
@@ -115,41 +170,73 @@ func user_login(in_username string, in_password string) (user ActiveUser, error 
 		if in_username == name {
 			// check password
 			h := salt_and_hash(salt, in_password, in_username)
+			fmt.Println("comparing hashed passwords: ", h, "and", hashed_password)
 			if h == hashed_password {
+				fmt.Println("match")
 				// user is verified
-				var u ActiveUser
+				var u User
 				u.Name = name
 				u.IsAdmin = (is_admin == "yes")
 
 				// generate a random unique cookie
-				u.Authcode = gen_rand_id(128)
+				u.Authcode = base64_random(128)
 				unique := false
 				for !unique {
 					unique = true
 					for i := 0; i < len(active_users); i++ {
 						if active_users[i].Authcode == u.Authcode {
 							unique = false
-							u.Authcode = gen_rand_id(128)
+							u.Authcode = base64_random(128)
 						}
 					}
 				}
 
-				fmt.Println("found user, made id: ", u.Authcode)
+				// fmt.Println("found user, made id: ", u.Authcode)
 
 				return u, nil
 			} else {
 				// not verified wrong password
-				return ActiveUser{}, errors.New("Wrong password")
+				return User{}, errors.New("Wrong password")
 			}
 		}
 	}
 
 	// on success add active user +
 	// on fail send error message
-	return ActiveUser{}, errors.New("Wrong username")
+	return User{}, errors.New("Wrong username")
+}
+
+func regcode_is_valid(code string) bool {
+	valid_codes := file_lines("registercodes.txt")
+
+	for i := 0; i < len(valid_codes); i++ {
+		if valid_codes[i] == code {
+			return true
+		}
+	}
+
+	return false
+}
+
+func regcode_add(code string) {
+	f, _ := os.OpenFile("registercodes.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+
+	f.Write([]byte(code + "\n"))
+
+	f.Close()
+}
+
+func regcode_remove(code string) {
+	c_data, _ := ioutil.ReadFile("registercodes.txt")
+
+	newdata := strings.Replace(string(c_data), code+"\n", "", 1)
+
+	ioutil.WriteFile("registercodes.txt", []byte(newdata), 0644)
 }
 
 func main() {
+	// regcode_add("lennoxisreal")
+	user_remove("richard")
 
 	_, err := user_login("richard", "password")
 	if err != nil {
@@ -164,6 +251,13 @@ func main() {
 	if err != nil {
 		fmt.Println("no tail file!")
 	}
+	fail_html, err := ioutil.ReadFile("templates/fail.html")
+	if err != nil {
+		fmt.Println("no fail file!")
+	}
+
+	fail_template := template.New("fail")
+	fail_template, err = fail_template.Parse(string(fail_html))
 
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
 
@@ -198,7 +292,6 @@ func main() {
 		enableCors(&w)
 
 		success_page, _ := ioutil.ReadFile("templates/success.html")
-		fail_page, _ := ioutil.ReadFile("templates/fail.html")
 
 		r.ParseForm()
 		username := r.PostForm.Get("username")
@@ -207,7 +300,7 @@ func main() {
 		user, err := user_login(username, password)
 
 		if err != nil {
-			w.Write(fail_page)
+			fail_template.Execute(w, "Wrong username or password, try again...")
 		} else {
 			// if logged in send cookie
 			c := http.Cookie{Name: "authcode", Value: user.Authcode, Expires: time.Now().Add(6 * time.Hour)}
@@ -220,11 +313,50 @@ func main() {
 			w.Write(success_page)
 		}
 	})
+	http.HandleFunc("/register", func(w http.ResponseWriter, r *http.Request) {
+		enableCors(&w)
+
+		success_page, _ := ioutil.ReadFile("templates/success.html")
+
+		r.ParseForm()
+		username := r.PostForm.Get("username")
+		code := r.PostForm.Get("code")
+		pass1 := r.PostForm.Get("password1")
+		pass2 := r.PostForm.Get("password2")
+
+		// is code valid
+		if !regcode_is_valid(code) {
+			// say code isn't valid
+			fail_template.Execute(w, "Registration code not valid, try again...")
+		} else if pass1 != pass2 {
+			// passwords don't match
+			fail_template.Execute(w, "Passwords didn't match, try again...")
+
+		} else if !user_is_name_unique(username) {
+			// is username taken
+			fail_template.Execute(w, "Username already in use, try again...")
+		} else {
+
+			// add the user
+			user_add(username, pass1, false, code)
+
+			user, _ := user_login(username, pass1)
+
+			c := http.Cookie{Name: "authcode", Value: user.Authcode, Expires: time.Now().Add(6 * time.Hour)}
+			c.SameSite = http.SameSiteStrictMode
+			http.SetCookie(w, &c)
+			active_users = append(active_users, user)
+
+			// show succesful login page
+			w.Write(success_page)
+			regcode_remove(code)
+		}
+	})
 	http.HandleFunc("/logout", func(w http.ResponseWriter, r *http.Request) {
 		enableCors(&w)
 
-		authcode := get_authcode(r)
-		user, err := get_user_from_authcode(authcode)
+		authcode := authcode_from_request(r)
+		user, err := user_from_authcode(authcode)
 
 		if err != nil {
 			// invalid but not possible in ui so do nothing
@@ -254,14 +386,35 @@ func main() {
 			w.Write(tail)
 		}
 	})
+	http.HandleFunc("/ui-admintable", func(w http.ResponseWriter, r *http.Request) {
+		enableCors(&w)
+
+		authcode := authcode_from_request(r)
+		user, err := user_from_authcode(authcode)
+
+		// check credentials
+		if err == nil && user.IsAdmin {
+			// show fellow admins just for kicks
+			fmt.Fprintf(w, "<ul>")
+			fmt.Fprintf(w, "</ul>")
+
+			fmt.Fprintf(w, "<ul>")
+			// list every non admin user, for deletion
+
+			// for i := 0; i < len(); i++ {
+			// 	fmt.Fprintf(w, "<li> <div class=\"x\" hx-trigger=\"click\" hx-post=\"/deluser?"+name+"\" hx-confirm=\"Are you sure you want to delete this user and all their posts?\"></div> </li>")
+			// }
+			fmt.Fprintf(w, "</ul>")
+		}
+	})
 	http.HandleFunc("/ui-loginbutton", func(w http.ResponseWriter, r *http.Request) {
 		enableCors(&w)
 
-		authcode := get_authcode(r)
-		user, err := get_user_from_authcode(authcode)
+		authcode := authcode_from_request(r)
+		user, err := user_from_authcode(authcode)
 
 		if err != nil {
-			fmt.Fprintf(w, "<a href=\"/login\" class=\"nav-item\">LOGIN</a>")
+			fmt.Fprintf(w, "<a href=\"/login\" class=\"nav-item\">LOGIN/REGISTER</a>")
 		} else {
 			if user.IsAdmin {
 				fmt.Fprintf(w, "<a href=\"/admin\" class=\"nav-item\">ADMIN</a>")
